a2006be5dffc40f3b4d7c368a19bf94abf261ba7
mtbroomell <mtbroomell@gmail.com>
2016-03-27 15:32:23 -0400

    Add Event Listener for Adding Species to Form

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
-/*
-*	Submission Script
-*/
+// Form Listener - dynamically add rows for amount of species
+var numSpeciesContainer = document.getElementById('numSpecies');
+numSpeciesContainer.addEventListener('change', function(){
+	var spec_num = numSpeciesContainer.value;
+	var i;
+	for(i=0; i<spec_num; i++) {
+		
+	}
+});
 
 // Get the form submission and add eventlistener to it
 var submit = document.getElementById('form_submit');
@@ -11,13 +17,16 @@ submit.addEventListener('click', function(){
 	var title = document.getElementById('title').value;
 	var runAmt = document.getElementById('runAmt').value;
 	var picksPerRun = document.getElementById('picksPerRun').value;
+	var numSpecies = document.getElementById('numSpecies').value;
+
 
 	// Create new Project Object
 	var projArgs = {
 		'name': name,
 		'title': title,
 		'runAmt': runAmt,
-		'picksPerRun': picksPerRun
+		'picksPerRun': picksPerRun,
+		'numSpecies': numSpecies
 	}
 
 	var project = new Project(projArgs);
