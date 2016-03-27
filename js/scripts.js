e89c8bec3c6d14571b6353357afdd30eac9eadf7
mtbroomell <mtbroomell@gmail.com>
2016-03-27 15:16:59 -0400

    Set new project object

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,27 @@
 *	Submission Script
 */
 
+// Get the form submission and add eventlistener to it
 var submit = document.getElementById('form_submit');
 
 submit.addEventListener('click', function(){
+	// Get Form Scope Vars & set them to Session Storage
 	var name = document.getElementById('name').value;
-	sessionStorage.setItem('klein_name', name);
+	var title = document.getElementById('title').value;
+	var runAmt = document.getElementById('runAmt').value;
+	var picksPerRun = document.getElementById('picksPerRun').value;
 
+	// Create new Project Object
+	var projArgs = {
+		'name': name,
+		'title': title,
+		'runAmt': runAmt,
+		'picksPerRun': picksPerRun
+	}
+
+	var project = new Project(projArgs);
+	console.log(project);
+
+	// Grab Results Container
 	var results = document.getElementById('results');
-	results.innerHTML = sessionStorage.getItem('klein_name');
 })
