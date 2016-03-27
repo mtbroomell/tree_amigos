5089c783d20ab5c8b6e65cee4ffcd7d38cdc866f
mtbroomell <mtbroomell@gmail.com>
2016-03-27 17:30:16 -0400

    Sets Session Storage Dynamically

--- js/scripts.js
+++ js/scripts.js
@@ -13,9 +13,8 @@ numSpeciesContainer.addEventListener('change', function(){
 		var spec_row_container = document.createElement('tr');
 		spec_row_container.innerHTML = spec_row;
 		spec_tgt.appendChild(spec_row_container);
+		spec_row_container.setAttribute('class','spec_row');
 		spec_row_container.id = "spec_row_" + (i+1);
-
-		console.log(spec_row_container);
 
 	}
 });
@@ -24,13 +23,12 @@ numSpeciesContainer.addEventListener('change', function(){
 var submit = document.getElementById('form_submit');
 
 submit.addEventListener('click', function(){
-	// Get Form Scope Vars & set them to Session Storage
+	// Get Form Scope Vars
 	var name = document.getElementById('name').value;
 	var title = document.getElementById('title').value;
 	var runAmt = document.getElementById('runAmt').value;
 	var picksPerRun = document.getElementById('picksPerRun').value;
 	var numSpecies = document.getElementById('numSpecies').value;
-
 
 	// Create new Project Object
 	var projArgs = {
@@ -40,10 +38,11 @@ submit.addEventListener('click', function(){
 		'picksPerRun': picksPerRun,
 		'numSpecies': numSpecies
 	}
+	console.log(projArgs);
 
-	var project = new Project(projArgs);
-	console.log(project);
-
-	// Grab Results Container
-	var results = document.getElementById('results');
+	// Store Project as SessionStorage
+	for ( var prop in projArgs ) {
+		console.log(prop);
+		sessionStorage.setItem(prop, projArgs[prop]);
+	}
 })
