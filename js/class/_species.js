2a5458a1fa42bc82edce1f1a61f345f4df5a53fc
mtbroomell <mtbroomell@gmail.com>
2016-03-29 22:10:41 -0400

    Sets up initial table parameters

--- js/class/_species.js
+++ js/class/_species.js
@@ -3,6 +3,7 @@
 */
 
 var Species = function(o){
+	this.sp_hit = 0;
 	this.sp_name = o.sp_name;
 	this.sp_low = o.sp_low;
 	this.sp_high = o.sp_high;
--- js/results.js
+++ js/results.js
@@ -17,21 +17,29 @@
 
 })(document, window);
 
-// Species Factory
-var species = [];
-for (var i = 0; i <= sessionStorage.length; ++i) {
-	spec_sub = 'species_';
-	var temp = sessionStorage.key(i);
-	if(typeof temp == 'string'){
-		if(temp.indexOf(spec_sub) > -1) {
-			var subTemp = sessionStorage.getItem(temp);
-			var subSplit = subTemp.split(", ");
-			var subTempObj = new Species({
-				'sp_name': subSplit[0],
-				'sp_low': subSplit[1],
-				'sp_high': subSplit[2]
-			});
-			species.push(subTempObj);
-		}	
+$(document).ready(function(){
+	// Project Info
+
+	// Species Factory
+	var species = [];
+	for (var i = 0; i <= sessionStorage.length; ++i) {
+		spec_sub = 'species_';
+		var temp = sessionStorage.key(i);
+		if(typeof temp == 'string'){
+			if(temp.indexOf(spec_sub) > -1) {
+				var subTemp = sessionStorage.getItem(temp);
+				var subSplit = subTemp.split(", ");
+				var subTempObj = new Species({
+					'sp_name': subSplit[0],
+					'sp_low': subSplit[1],
+					'sp_high': subSplit[2]
+				});
+				species.push(subTempObj);
+			}	
+		}
 	}
-}
+	for(var i = 0; i < species.length; i++) {
+		var row = $("<td>" + species[i].sp_name + "</td><td>" + species[i].sp_hit + "</td>");
+		$('#results-table tbody').append(row);
+	}
+});
